8f15cee3668541404ba512f0aa72f28eb351cb85
luizao <luizsantiago280@gmail.com>
2024-10-14 20:43:18 -0300

    comit1

---
+++ index.js
@@ -0,0 +1,13 @@
+let valor = document.getElementById("valor");
+let form = document.getElementById("formulario");
+let resultado = document.getElementById("resultado");  
+
+form.addEventListener('submit', function(e) {
+    let valorInput = valor.value;
+    
+    
+   
+    e.preventDefault();
+});
+
+
